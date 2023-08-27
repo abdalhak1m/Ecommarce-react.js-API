cde06ee1c5b0afd26d42db6111643aa76095594b
abdalhak1m <abdalhakimalhallak6@gmail.com>
2023-08-27 06:37:00 -0700

    Cart Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Home from "../pages/HomePage/HomePage";
+import Cart from "../pages/CartPage/CartPage";
 import CategoryProduct from "../pages/CategoryProductPage/CategoryProductPage";
 import ProductSingle from "./ProductSinglePage/ProductSinglePage";
 import Search from "./SearchPage/SearchPage";
